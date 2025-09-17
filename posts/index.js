addd9e40a8fe4e2ddf68ae768120f320637badae
zedaenious <matt@zelenak.me>
2025-09-17 18:29:00 -0400

    init GET/POST for /posts route, in local mem

--- posts/index.js
+++ posts/index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
 
 const app = express();
+app.use(bodyParser.json());
+
+// storing all data in local memory
+const posts = {};
 
 app.get('/posts', (req, res) => {
-
+  res.send(posts);
 });
 
 app.post('/posts', (req, res) => {
-  
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
