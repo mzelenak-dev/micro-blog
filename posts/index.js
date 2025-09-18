154fc1332085ed2b04baf20d4c9f5885f5a76995
zedaenious <matt@zelenak.me>
2025-09-17 22:03:16 -0400

    enable CORS for /posts /comments routes

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 // storing all data in local memory
 const posts = {};
