54efa100495777ce9c5abbad1a0ca0bf5b86e169
zedaenious <matt@zelenak.me>
2025-09-17 21:57:19 -0400

    PostCreate component; axios call fails CORS

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+// import React from "react";
+import PostCreate from "./PostCreate";
+
+const App = () => {
+  return (
+    <PostCreate title="Create Post" />
+  )
+};
+
+export default App;
---
+++ client/src/PostCreate.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { useState } from "react";
+
+const PostCreate = ({title}) => {
+  const [postContent, setPostContent] = useState('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post('http://localhost:4000/posts', {
+      postContent
+    });
+  };
+
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Title</label>
+          <input 
+            type="text"
+            value={postContent}
+            className="form-control"
+            onChange={(e) => setPostContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  )
+};
+
+export default PostCreate;
