4b005fe30c94315f681b8e5919f21e47aaea6440
zedaenious <matt@zelenak.me>
2025-09-22 11:53:21 -0400

    add moderation app to run-all startup

---
+++ moderation/index.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {});
+
+app.listen(4003, () => { console.log('MODERATION SRVC RUNNING ON 4003')});
