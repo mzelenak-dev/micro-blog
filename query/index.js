8ebf214e8a78afb83d97ac787e461c6977d0663b
zedaenious <matt@zelenak.me>
2025-09-24 22:35:24 -0400

    QUERY now syncs EVBUS data on app start

--- query/index.js
+++ query/index.js
@@ -9,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const {type, data} = req.body;
-  
+const handleEvent = (type, data) => {
   if(type === 'PostCreated') {
     const {id, postContent} = data;
     posts[id] = { id, postContent, comments: [] };
@@ -34,10 +28,31 @@ app.post('/events', (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+}
 
-  console.log(posts);
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const {type, data} = req.body;
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => { console.log('QUERY SRVC ON 4002') })
+app.listen(4002, async () => {
+  console.log('QUERY SRVC ON 4002');
+  
+  // try to pull all existing events from EVBUS data store
+  try {
+    const res = await axios.get('http://localhost:4005/events');
+
+    for(let event of res.data) {
+      console.log(`Processing event ${JSON.stringify(event)}`);
+      handleEvent(event.type, event.data);
+    }
+  } catch(err) {
+    console.error(err);
+  }
+})
