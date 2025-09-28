986b41a5c2fc22f49c52762f9daabdb8a70aa908
zedaenious <matt@zelenak.me>
2025-09-27 22:12:43 -0400

    setting default Docker files for each srvc

--- posts/index.js
+++ posts/index.js
@@ -37,4 +37,7 @@ app.post('/events', (req, res) => {
   res.send({});
 });
 
-app.listen(4000, () => {console.log('POSTS SRVC ON 4000')})
+app.listen(4000, () => {
+  console.log('POSTS SRVC ON 4000');
+  console.log('v2');
+});
